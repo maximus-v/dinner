a0bc7113776d0bc6b6a7caccfc15f8b9c8f72d96
Max Vorhauer <max.vorhauer@descript.de>
2020-10-09 12:22:49 +0200

    Styling of views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,47 @@
 <template>
-  <b-container>
-    <h1>Guten Abend {{ name }}</h1>
+  <b-row class="w-100 mx-auto text-center h-100">
 
-    <b-card title="Vorspeise">
-      <b-card-text>
-        Nibbles mit Kiwi-Dip
-      </b-card-text>
-      <b-card-text>
-        Fritata mit verschiedenem Gemüse
-      </b-card-text>
-      <b-card-text>
-        Garnelencocktails
-      </b-card-text>
-    </b-card>
+    <b-col cols="12" class="mt-4 mb-2">
+      <div>
+        <h1>Guten Abend {{ name }}</h1>
+        <p>Folgendes Menü erwartet dich heute Abend. <br>Ich hoffe du hast Spaß.</p>
+      </div>
+    </b-col>
 
-    <b-card title="Hauptgang">
-      <b-card-text>
-        Fish & Chips
-      </b-card-text>
-      <b-card-text>
-        verschiedene Dips (Mushy Peas, Guacamole, Limetten-Mayo)
-      </b-card-text>
-    </b-card>
+    <b-col cols="12" class="my-2">
+      <b-card title="Vorspeise">
+        <b-card-text>
+          Nibbles mit Kiwi-Dip
+        </b-card-text>
+        <b-card-text>
+          Fritata mit verschiedenem Gemüse
+        </b-card-text>
+      </b-card>
+    </b-col>
 
-    <b-card title="Nachspeise">
-      <b-card-text>
-        Churros mit Schokoladensoße
-      </b-card-text>
-      <b-card-text>
-        verschiedenes Obst
-      </b-card-text>
-    </b-card>
-  </b-container>
+    <b-col cols="12" class="my-2">
+      <b-card title="Hauptgang">
+        <b-card-text>
+          Fish & Chips
+        </b-card-text>
+        <b-card-text>
+          verschiedene Dips (Mushy Peas, Guacamole, Limetten-Mayo)
+        </b-card-text>
+      </b-card>
+    </b-col>
+
+    <b-col cols="12" class="my-2">
+      <b-card title="Nachspeise">
+        <b-card-text>
+          Churros mit Schokoladensoße
+        </b-card-text>
+        <b-card-text>
+          verschiedenes Obst
+        </b-card-text>
+      </b-card>
+    </b-col>
+
+  </b-row>
 </template>
 
 <script>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,27 +1,43 @@
 <template>
   <b-container>
-    <b-form @submit="onSubmit">
-      <b-form-group id="name-group" label="Name" label-for="name">
-        <b-form-input
-          id="name"
-          v-model="form.name"
-          type="text"
-          required
-          placeholder="Gib deinen Namen ein"
-        ></b-form-input>
-      </b-form-group>
+    <b-row class="w-100 mx-auto text-center h-100">
 
-      <b-form-group id="sign-group" label="Sternzeichen:" label-for="sign">
-        <b-form-select
-          id="sign"
-          v-model="form.sign"
-          :options="signs"
-          required
-        ></b-form-select>
-      </b-form-group>
+      <b-col cols="12" class="mt-4 mb-2">
+        <div>
+          <h1>Gästeregistrierung</h1>
+          <p>Auf Grund der aktuellen Corona-Lage musst du dich registrieren,
+            damit im Zweifel das Gesundheitsamt Infektionsketten nachvollziehen
+            kann (oder der Staat dich überwachen kann).</p>
+        </div>
+      </b-col>
 
-      <b-button type="submit" variant="primary">Bestätigen</b-button>
-    </b-form>
+      <b-col cols="12" class="my-2 text-left">
+        <b-form @submit="onSubmit">
+          <b-form-group id="name-group" label="Name" label-for="name">
+            <b-form-input
+              id="name"
+              v-model="form.name"
+              type="text"
+              required
+              placeholder="Gib deinen Namen ein"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="sign-group" label="Sternzeichen:" label-for="sign">
+            <b-form-select
+              id="sign"
+              v-model="form.sign"
+              :options="signs"
+              required
+            ></b-form-select>
+          </b-form-group>
+
+          <b-button type="submit" variant="light" class="mt-4">Bestätigen</b-button>
+        </b-form>
+      </b-col>
+
+    </b-row>
+
   </b-container>
 </template>
 
@@ -48,6 +64,14 @@ export default {
         'Schütze',
         'Steinbock'
       ]
+    }
+  },
+  mounted () {
+    if (localStorage.name) {
+      this.form.name = localStorage.name;
+    }
+    if (localStorage.sign) {
+      this.form.sign = localStorage.sign;
     }
   },
   methods: {
